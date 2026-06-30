327477fee0a34bed90d1ed87c1839e0ce8cf3be6
TestGrid Admin <info@testgrid.io>
2026-07-01 03:02:59 +0530

    Initial Version Commit

---
+++ compile-java-executable/649/testfunctions/TF0002.java
@@ -0,0 +1,29 @@
+import java.util.scanner;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+class tf0002 {
+
+	public static void tf0002() {
+		tg.wait("ele_dfsdf", ComparisonType.IS_VISIBLE, 5);
+		tg.wait(4);
+	}
+}
