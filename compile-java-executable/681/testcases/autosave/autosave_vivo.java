7130ca637e8c5343154f048a1b80d6243e48235e
TestGrid Admin <info@testgrid.io>
2026-07-10 00:59:56 +0530

    Initial Version Commit

---
+++ compile-java-executable/681/testcases/autosave/autosave_vivo.java
@@ -0,0 +1,34 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class vivo {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void vivo() {
+		tg.openBrowser();
+				tg.wait("ele_logintofac715", ComparisonType.IS_VISIBLE);
+				tg.click("ele_logintofac715", 1);
+				tg.wait("ele_element057", ComparisonType.IS_VISIBLE);
+				tg.click("ele_element057", 1);
+		tg.close();
+	}
+}
